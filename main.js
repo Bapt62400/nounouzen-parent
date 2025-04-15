f2329f6d95be4336d7d941606575bfaef8aeff8f
Bapt62400 <bapt.behague@gmail.com>
2025-04-15 21:33:36 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+// Fichier JS React simulé
+document.getElementById("root").innerHTML = `
+  <h1>Nounou Zen</h1>
+  <p>Bienvenue sur votre vraie appli React !</p>
+  <p>Navigation, profils, favoris, filtres à venir...</p>
+`;
